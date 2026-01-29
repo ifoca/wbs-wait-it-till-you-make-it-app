22857f2ea104c42733bed580e7ac0c7844928f76
bethel ike <betheluzonduike@gmail.com>
2026-01-29 15:23:07 +0100

    added the all the userContrller routes

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -8,3 +8,55 @@ export const getUsers: RequestHandler = async (req, res) => {
   }
   return res.status(200).json(users);
 };
+// get user by id 
+
+
+export const getUserById: RequestHandler =async(req,res)=>{
+  const user = await Users.findById(req.params.id);
+  if (!user){
+    throw new Error('user not found', { cause: 404 });
+
+  }
+  return res.status(200).json(user);
+};
+
+//post register user
+
+
+export const registerUser:RequestHandler= async(req, res)=>{
+  const user =await Users.create(req.body);
+  if (!user){
+    throw new Error('User registration failed', {cause:400});
+  }
+  return res.status(201).json({message:'User registered successfully'});
+};
+
+
+//post login user
+
+export const loginUser:RequestHandler = async(req,res)=>{
+  const {email, password}=req.body;
+  const user =await Users.findOne({email, password});
+  if (!user || user.password !== password){
+    throw new Error( 'invalid email or password',{cause:401});
+    // we all need to handle token here 
+  }
+  return res.status(200).json({message:'login successfully'});
+}
+
+//post logout user
+
+export const logoutUser:RequestHandler = async(req ,res)=>{
+  // we need to hand token here
+  return res.status(200).json({message:'logged out successfully'})
+}
+
+// delete user
+ 
+export const deleteUser:RequestHandler = async(req,res)=>{
+  const user =await Users.findByIdAndDelete(req.params.id);
+  if (!user){
+    throw new Error ('user not registered or found',{cause:404});
+  }
+  return  res.status(200).json({message:'account deleted successfully'})
+};
--- server/src/routes/usersRouter.ts
+++ server/src/routes/usersRouter.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { getUsers } from '#controllers';
+import { getUsers,getUserById, registerUser,loginUser,logoutUser,deleteUser } from '#controllers';
 
 const usersRouter = Router();
 
 // users
 usersRouter.get('/', getUsers);
+usersRouter.get('/user:id', getUserById);
+usersRouter.post('register', registerUser);
+usersRouter.post('/login', loginUser);
+usersRouter.post('/logout',logoutUser)
+usersRouter.delete('/user/:id', deleteUser)
 
 export default usersRouter;
