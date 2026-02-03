15681d8a355eb2c59defb89b03b8f5ba1dae14ec
ifoca <foca.ionut@gmail.com>
2026-02-03 10:02:03 +0100

    add loading message component

--- client/src/components/ErrorMessage.tsx
+++ client/src/components/ErrorMessage.tsx
@@ -1,9 +1,7 @@
 function ErrorMessage({ error }) {
   return (
     <div className="m-auto w-2/3">
-      <p className="text-center text-lg p-2 mt-4 border">
-        There was an error. <strong>{error}</strong>
-      </p>
+      <p className="text-center text-lg p-2 mt-4 border">There was an error. {error}</p>
     </div>
   );
 }
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Navbar } from './Navbar.tsx';
 export { default as TimetableItem } from './TimetableItem.tsx';
 export { default as SearchBar } from './SearchBar.tsx';
 export { default as ErrorMessage } from './ErrorMessage.tsx';
+export { default as LoadingMessage } from './LoadingMessage.tsx';
