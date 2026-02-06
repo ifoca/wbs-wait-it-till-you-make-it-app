ca6da6705a7bf20e1c9d7fb94f44e0d51437898e
bethel ike <betheluzonduike@gmail.com>
2026-02-06 17:05:04 +0100

    updated the errorhandler

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -3,6 +3,6 @@ import type { ErrorRequestHandler } from 'express';
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.log(err);
 
-  res.status(500).json({ message: 'something went wrong' });
+  res.status(500).json({ message: err.message });
 };
 export default errorHandler;
