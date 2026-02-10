bd4b45180aea95ffc715c3daa155389534c4bd90
ifoca <foca.ionut@gmail.com>
2026-02-10 11:52:06 +0100

    add schema types

--- server/src/schemas/userSchema.ts
+++ server/src/schemas/userSchema.ts
@@ -1,22 +1,25 @@
-import {z} from "zod/v4"
+import { z } from 'zod/v4';
 
- const usernameSchema = z.string({error: ' username must be a string'})
- const emailSchema = z.string({error: 'please input a valid  email'});
- const passwordSchema = z.string({error: 'must be a string'})
-                         .min(6,{error: 'must be at least 6 characters'});
+const usernameSchema = z.string({ error: ' username must be a string' });
+const emailSchema = z.string({ error: 'please input a valid  email' });
+const passwordSchema = z
+  .string({ error: 'must be a string' })
+  .min(6, { error: 'must be at least 6 characters' });
 
-
- export  const registrationSchema = z.object({
-    username:usernameSchema,
+export const registrationSchema = z
+  .object({
+    username: usernameSchema,
     email: emailSchema,
-    password:passwordSchema
+    password: passwordSchema,
+  })
+  .strict();
 
- })
- .strict();
+export const loginSchema = z
+  .object({
+    email: emailSchema,
+    password: passwordSchema,
+  })
+  .strict();
 
-
- export const loginSchema = z.object({
-    email:emailSchema,
-    password:passwordSchema
- })
- .strict()
+export type RegistrationSchema = z.infer<typeof registrationSchema>;
+export type LoginSchema = z.infer<typeof loginSchema>;
