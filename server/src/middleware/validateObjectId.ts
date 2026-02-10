8ee6c80b627e36fcf4a2dca3ef4e0b160518c330
ifoca <foca.ionut@gmail.com>
2026-02-10 18:01:58 +0100

    add object id validation middleware'

---
+++ server/src/middleware/validateObjectId.ts
@@ -0,0 +1,24 @@
+import { type RequestHandler } from 'express';
+import { Types } from 'mongoose';
+
+export const validateObjectId = (paramName: string = 'id'): RequestHandler => {
+  return (req, res, next) => {
+    const value = req.params[paramName];
+
+    if (typeof value !== 'string') {
+      return res.status(400).json({
+        message: `Missing or invalid ${paramName}`,
+      });
+    }
+
+    if (!Types.ObjectId.isValid(value)) {
+      return res.status(400).json({
+        message: `Invalid ${paramName} format`,
+      });
+    }
+
+    next();
+  };
+};
+
+export default validateObjectId;
