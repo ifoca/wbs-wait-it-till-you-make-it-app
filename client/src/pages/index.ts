2cef046da162f23fe48a863ca8d2e72c77220375
ifoca <foca.ionut@gmail.com>
2026-02-16 14:18:22 +0100

    remove import

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,3 @@
-export { default as Departures } from '../layouts/DeparturesLayout.tsx';
 export { default as Login } from './Login.tsx';
 export { default as Register } from './Register.tsx';
 export { default as UserProfile } from './UserProfile.tsx';
