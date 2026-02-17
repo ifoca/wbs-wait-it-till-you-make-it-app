1c28e9a29ab0fb8b1d2ed42570670276f99654a7
ifoca <foca.ionut@gmail.com>
2026-02-17 10:41:39 +0100

    trigger search on click

--- client/src/components/FavoriteItem.tsx
+++ client/src/components/FavoriteItem.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import type { Favorite } from '../types';
 
 type FavoriteItemProps = {
@@ -5,12 +6,20 @@ type FavoriteItemProps = {
 };
 
 const FavoriteItem = ({ favorite }: FavoriteItemProps) => {
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    navigate(`/search/${favorite.stationId.cityName}/${favorite.stationId.stationName}`);
+  };
+
   return (
     <tr className="hover:bg-base-100">
       <td>{favorite.stationId.cityName}</td>
       <td>{favorite.stationId.stationName}</td>
       <td>
-        <button className="btn btn-xs hover:bg-base-300">Departures</button>
+        <button onClick={handleSearch} className="btn btn-xs hover:bg-base-300">
+          Departures
+        </button>
       </td>
       <td>
         <button className="btn btn-xs hover:bg-base-300">Remove</button>
