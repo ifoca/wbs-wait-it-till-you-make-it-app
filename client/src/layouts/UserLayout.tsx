325d918c7260d54bd92cfaf288c4f21871f24f35
ifoca <foca.ionut@gmail.com>
2026-02-18 10:49:45 +0100

    polish search bar, layouts

--- client/src/layouts/UserLayout.tsx
+++ client/src/layouts/UserLayout.tsx
@@ -12,16 +12,8 @@ const UserLayout = () => {
 
   return (
     <div className="flex-1 bg-base-100 text-neutral-content">
-      <div className="p-4 text-center">
-        <h1 className="text-3xl font-bold px-4">User page</h1>
-        <p className="text-sm px-4">
-          Here we are going to implement the user authentication and profile management.
-        </p>
-      </div>
       <div className="flex flex-col gap-8 p-4">
-        <div>
-          <Outlet />
-        </div>
+        <Outlet />
       </div>
     </div>
   );
